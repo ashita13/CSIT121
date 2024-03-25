fcc0e2eceadf44009e05c7ed22ed859a1d8ed15e
ashita13 <nguyenquyhiep13@gmail.com>
2024-03-25 20:30:05 +1100

    adding show organization with role function, editing class Organization

--- menu/system/organization/organization.py
+++ menu/system/organization/organization.py
@@ -1,5 +1,22 @@
+# create a dict to store organizations name as key
+# and role as value
 class Organization:
     def __init__(self):
-        self.organizations_list = []
-        
-   
+        self.role = {}
+    
+    # add role value by organization name
+    def add_role(self, organ_name, role):
+        role_name = self.role.get(role)
+        if role_name not in self.role:
+            self.role[organ_name] = []
+        # append to value of key so this key's value is a list
+        self.role[organ_name].append(role)
+
+    # get the according role with organization name
+    def get_role(self, organ_name):
+        output_role = self.role.get(organ_name)
+        if output_role:
+            return "".join(output_role)
+        else: return "None"
+    
+
