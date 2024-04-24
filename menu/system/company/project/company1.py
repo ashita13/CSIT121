0cabf203a127cd0789cd4085d0e1d3dac2a2ad81
ashita13 <nguyenquyhiep13@gmail.com>
2024-04-24 11:44:21 +1000

    write function for Company class

--- menu/system/company/project/company1.py
+++ menu/system/company/project/company1.py
@@ -1,3 +1,11 @@
 class Company:
-    def __init__(self):
-        pass
+    
+    def __init__(self, company_name):
+        self.company_name = company_name
+        self.company_list = {}
+    
+    def record_company(self, project_title):
+        if project_title not in self.company_list:
+            self.company_list[project_title] = []
+        self.company_list[project_title].append(self.company_name)
+    
